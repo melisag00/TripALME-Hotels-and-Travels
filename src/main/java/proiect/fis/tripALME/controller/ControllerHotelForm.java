a73a31269c8b0a847eb5eafca4677ee69cf2a4e3
Darau <daraualexandru@gmail.com>
2020-05-04 12:25:59 +0300

    THT-21 #comment Create a GUI Register Form

---
+++ src/main/java/proiect/fis/tripALME/controller/ControllerHotelForm.java
@@ -0,0 +1,30 @@
+package proiect.fis.tripALME.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class ControllerHotelForm {
+
+    @FXML
+    private TextField username;
+
+    @FXML
+    private PasswordField password;
+
+    @FXML
+    private TextField email;
+
+    @FXML
+    private TextField hotelName;
+
+    @FXML
+    private TextField address;
+
+    @FXML
+    void register(ActionEvent event) {
+
+    }
+
+}
