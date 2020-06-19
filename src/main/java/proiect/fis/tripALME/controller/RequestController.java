edbc9c38421c37a4c360b44e4d3cc9cca67b0da6
Melisa <melisa.galescu@gmail.com>
2020-06-19 23:33:36 +0300

    Updated

--- src/main/java/proiect/fis/tripALME/controller/RequestController.java
+++ src/main/java/proiect/fis/tripALME/controller/RequestController.java
@@ -1,15 +1,17 @@
 package proiect.fis.tripALME.controller;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.text.Text;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
-import java.io.BufferedReader;
+
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.Reader;
@@ -24,6 +26,8 @@ public class RequestController {
     @FXML
     private ListView<String> List1;
 
+    @FXML
+    private AnchorPane go;
 
     @FXML
     private Text message;
@@ -35,8 +39,12 @@ public class RequestController {
     ArrayList<String> list1 = new ArrayList<String>();
     JSONParser parser1 = new JSONParser();
     JSONParser parser2 = new JSONParser();
+    JSONParser parser3 = new JSONParser();
+    JSONParser parser4 = new JSONParser();
     JSONObject list11 = new JSONObject();
-    String username = "";
+    private String username = "";
+    private String user = "";
+    private String hotel = "";
 
     public void initialize() {
 
@@ -50,8 +58,24 @@ public class RequestController {
         } catch (ParseException e) {
             e.printStackTrace();
         }
+        try (Reader reader = new FileReader("src/main/java/data/data.json")) {
+            JSONArray jsonArray = (JSONArray) parser3.parse(reader);
+            Iterator<JSONObject> it = jsonArray.iterator();
+            while (it.hasNext()) {
+                JSONObject obj = it.next();
+                user = obj.get("username").toString();
+                if(user.equals(username))
+                    hotel = obj.get("hotelName").toString();
+                break;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
         try (Reader reader = new FileReader("src/main/java/data/rooms.json")) {
-            JSONArray jsonArray = (JSONArray) parser1.parse(reader);
+            JSONArray jsonArray = (JSONArray) parser4.parse(reader);
             Iterator<JSONObject> it = jsonArray.iterator();
             while (it.hasNext()) {
                 JSONObject obj = it.next();
@@ -65,13 +89,17 @@ public class RequestController {
         } catch (ParseException e) {
             e.printStackTrace();
         }
+
         List.getItems().addAll(list);
+
         try (Reader reader = new FileReader("src/main/java/data/request.json")) {
             JSONArray jsonArray = (JSONArray) parser2.parse(reader);
             Iterator<JSONObject> it = jsonArray.iterator();
             while (it.hasNext()) {
                 JSONObject obj = it.next();
                 String x = obj.get("request").toString();
+                String y = obj.get("hotel").toString();
+                if(y.equals(hotel))
                     list1.add(x);
             }
         } catch (IOException e) {
@@ -103,6 +131,16 @@ public class RequestController {
             e.printStackTrace();
         } catch (ParseException e) {
             e.printStackTrace();
+        }
+    }
+
+    @FXML
+    void Back() {
+        try {
+            AnchorPane pane = FXMLLoader.load(getClass().getClassLoader().getResource("Manager.fxml"));
+            go.getChildren().setAll(pane);
+        } catch (Exception e) {
+            System.out.println("Cant load the window");
         }
 
     }
